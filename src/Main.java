b41b324dac0a4a3363f9e8a070c735a20899a6b3
MalikNegmatov <maliknegmatov@gmail.com>
2023-04-21 13:10:49 +0300

    First pushh

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+////      Задачи по ссылке https://skyengpublic.notion.site/1-2-070c491064914f4b8608fa55a3424472
+////      Решения ниже в методах 1.n.m Здесь тесты и проверка.
+//
+//        System.out.println("\nЗадача №1.4.4");
+//        task1_4_4(3, 5, 30);
+//        task1_4_4(3, 7, 100);
+//// -------------------------------------------------------------------------------
+//        System.out.println("\nЗадача №1.4.5");
+//        task1_4_5(10);
+//        System.out.println();
+//        task1_4_5(20);
+//        System.out.println();
+//    }
+//}
